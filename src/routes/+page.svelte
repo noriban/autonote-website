b4a4dab8c73e264e364da5f8f9e3e8afe8e87864
Norbert <norbertrom01@gmail.com>
2023-01-25 08:09:47 +0000

    new navbar on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,32 @@
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Adlam+Unjoined:wght@400;500;600&display=swap');
-body {
+* {
     font-family: 'Noto Sans Adlam Unjoined', sans-serif;
     padding: 0;
     margin: 0;
 }
 nav {
-    margin: 0.3em;
-    border: 1px solid blue;
-    height: 70%;
+    /* border: 1px solid red; */
+    margin-bottom: 0.3em;
+    margin-top: 0.3em;
+    border: 0.1em solid gray;
+    height: 20%;
     width: 70%;
-    border-radius: 0.6em;
-    display: block;
+    border-radius: 5em;
+    left: 15%;
+    position: relative;
     place-content: center;
     align-items: center;
 }
 nav ul {
-    border: 1px solid green;
-    padding: 1em;
-    margin: 1em;
+    /* border: 1px solid green; */
+    /* padding: 1em; */
+    margin: 0.5em;
     align-items: center;
     place-content: center;
     display: flex;
     justify-content: space-evenly;
+    border-radius: 15px;
 }
 /* nav ul a {
     border: 1px solid purple;
@@ -30,7 +34,7 @@ nav ul {
     align-items: center;
 } */
 nav ul a{
-    border: 1px solid red;
+    /* border: 1px solid red; */
     width: 8em;
     height: 2em;
     margin: 0;
@@ -38,24 +42,25 @@ nav ul a{
     text-align: center;
     align-items: center;
     place-content: center;
+    border-radius: 10px;
     
     /* display: inline-block; */
     /* height: inherit;
     width: inherit; */
     list-style: none;
 }
-nav ul a:link {
+nav ul a:visited, nav ul a:link {
     text-decoration: none;
+    color: inherit;
 }
-nav ul a:visited {
-    text-decoration: none;
+nav ul a:active {
+    border-radius: 10px;
 }
 nav ul a:hover {
+    color: white;
+    background-color: black;
+    /* animation-duration: 0.15s; */
     transition: 0.15s;
-    /* color: black; */
-    background-color: aqua;
-    color: red;
-    animation-duration: 0.15s;
     border-radius: 15px;
 }
 section {
@@ -68,14 +73,26 @@ section {
 </style>
 <nav>
     <ul>
-        <a href="/about">
-            <li>about</li>
-        </a><a href="/">
-            <li>test</li>
-        </a><a href="/">
+        <a href="/">
+            <li>Home</li>
+        </a><a href="/features">
+            <li>Features</li>
+        </a><a href="/test">
             <li>test</li>
         </a><a href="/">
             <li>test</li>
         </a>
     </ul>
 </nav>
+<section>
+    Hi, im james
+</section>
+<section>
+    Hi, im james
+</section>
+<section>
+    Hi, im james
+</section>
+<section>
+    Hi, im james
+</section>
