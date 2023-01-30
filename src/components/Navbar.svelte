691730c5299bf70f99b8777e29058ddd2503b625
Norbert <norbertrom01@gmail.com>
2023-01-30 20:28:08 +0000

    Still upgrading navbar with new media sizes and new button layout

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,12 +1,14 @@
 <script>
     import logo from '../assets/logo.png'
     import '../styles/navbar.css'
+
 </script>
-<header>
+<header class="test">
     <img class="logo" src="{logo}" alt="logo">
-    <input class="side-menu" type="checkbox" id="side-menu">
-    <label class="hamb" for="side-menu"><span class="hamb-line"></span></label>
-    <nav>
+    <div class="toggle">
+        <a><i class="fa fa-bars">v</i></a>
+    </div>
+    <nav class="menu">
         <ul class="nav__links">
             <li><a href="#features">Features</a></li>
             <li><a href="#faq">FAQ</a></li>
@@ -14,5 +16,5 @@
             <li><a href="https://github.com/noriban/AutoNote">Source</a></li>
         </ul>
     </nav>
-    <a href="https://github.com/noriban/AutoNote/releases" class="cta"><button>Download</button></a>
+    <a href="https://github.com/noriban/AutoNote/releases" class="cta"><button class="button-85" role="button">Download</button></a>
 </header>
