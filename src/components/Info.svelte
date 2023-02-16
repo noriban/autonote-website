67643f2949cf0353b8a595c2d0256ee157648086
Norbert <norbertrom01@gmail.com>
2023-02-16 18:56:49 +0000

    `style` : add new info section

--- src/components/Info.svelte
+++ src/components/Info.svelte
@@ -1,28 +1,30 @@
 <script>
     import '../styles/card.css'
 </script>
-<div class="container mx-auto px-10 py-16 text-center">
+<div class="container mx-auto px-10 py-10 text-center">
     <div class="mx-auto max-w-lg">
     <h1 class="text-5xl font-bold text-gray-800 dark:text-white lg:text-7xl">AutoNote</h1>
     <p class="mt-3 text-xl text-gray-500 dark:text-gray-300 lg:text-3xl">Your car companion</p>
+    <img class="rounded-full items-center mx-auto mt-5" alt="GitHub release (latest by date)" src="https://img.shields.io/github/v/release/noriban/autonote?label=VERSION&logo=android&style=for-the-badge">
     </div>
 </div>
-<div class="container mx-auto px-2 py-10">
+<div class="container mx-auto px-2 py-2">
 
 
-    <div class="mt-1 grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-2 xl:gap-14 xl:items-center xl:justify-center">
+    <div class="mt-1 grid grid-cols-1 mx-4 gap-2 md:grid-cols-2 xl:grid-cols-2 xl:gap-14 xl:items-center xl:justify-center">
         <div class="info_content">
         <div class="card">
-            <div class="card2 text-white text-center">
-                test
+            <div class="card2 text-center mx-auto p-5">
+                <p class="titles">Save money</p>
+                <p class="card_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum adipisci, libero sunt quod dolor porro culpa est maxime iusto quasi suscipit fuga numquam eaque a. Sequi voluptatem dolores quae repudiandae?</p>
             </div>
         </div>
     </div>
     <div class="info_content">
         <div class="card">
-            <div class="card2 text-white text-center justify-center items-center">
-                <p>test</p>
-                <img class="rounded-full" alt="GitHub release (latest by date)" src="https://img.shields.io/github/v/release/noriban/autonote?logo=android&style=flat-square">
+            <div class="card2 text-center justify-center items-center mx-auto p-5">
+                <p class="titles">Remember</p>
+                
             </div>
         </div>
     </div>
