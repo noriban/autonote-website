0aab85b2ee4998e2ac4c5f00497c940d44a63773
Norbert <norbertrom01@gmail.com>
2023-02-16 20:08:05 +0000

    `fix` : change navbar on tablets

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -27,7 +27,7 @@
     </a>
 </header> -->
 
-<nav class="bg-gray-200 shadow shadow-gray-300 w-100 sm:py-4 md:py-0 md:w-3/4 md:mx-auto md:px-auto md:rounded-full md:my-3 xl:w-2/4">
+<nav class="bg-gray-200 shadow shadow-gray-300 w-100 sm:py-4 md:py-0 md:w-4/5 lg:w-3/4 md:mx-auto md:px-auto md:rounded-full md:my-3 xl:w-2/4">
 	<div class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap ">
 		<!-- Logo -->
 		<div class="mx-1 md:mx-0 md:order-1">
@@ -49,9 +49,9 @@
 		<div class="order-2 md:order-3">
       <a href="https://github.com/noriban/AutoNote/releases">
 
-        <button class="downButton px-3 py-4 shadow-2xl bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2 md:py-3 hover:py-4">
+        <button class="downButton px-3 py-4 sm:px-1 shadow-2xl bg-indigo-500 hover:bg-indigo-600 text-gray-50 rounded-xl flex items-center gap-2 md:py-3 hover:py-4">
         <!-- Heroicons - Login Solid -->
-        <span>Download</span>
+        <span class="">Download</span>
       </button>
       </a>
 		</div>
