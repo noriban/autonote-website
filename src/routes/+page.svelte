1043be1e94f5f25579268c5a47c1915425d5099d
Norbert <norbertrom01@gmail.com>
2023-02-16 08:03:16 +0000

    `feat` : add components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,22 @@
     import Navbar from '../components/Navbar.svelte'
     import Info from '../components/Info.svelte'
     import FooterComp from '../components/Footer.svelte'
+    import Features from '../components/Features.svelte'
+    import FAQ from '../components/Faq.svelte'
+
 </script>
+
 <Navbar/>
 
 <main>
-    <section>
+    <section class="min-h-screen">
         <Info />
     </section>
     <section>
-        
+        <Features />
     </section>
     <section>
-
+        <!-- <Faq/> -->
     </section>
 </main>
 <FooterComp />
