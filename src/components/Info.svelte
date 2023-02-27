190bbb169e0dcd6e1facc40913f1aa251b957b16
Norbert <norbertrom01@gmail.com>
2023-02-27 21:00:02 +0000

    add releases version

--- src/components/Info.svelte
+++ src/components/Info.svelte
@@ -3,15 +3,23 @@
 </script>
 <div class="container mx-auto px-10 py-10 text-center">
     <div class="mx-auto max-w-lg">
-    <h1 class="text-5xl font-bold text-gray-800 dark:text-white lg:text-7xl">AutoNote</h1>
-    <p class="mt-3 text-xl text-gray-500 dark:text-gray-300 lg:text-3xl">Your car companion</p>
-    <img class="rounded-full items-center mx-auto mt-5" alt="GitHub release (latest by date)" src="https://img.shields.io/github/v/release/noriban/autonote?label=VERSION&logo=android&style=for-the-badge">
-    </div>
+        <div class="flex justify-center">
+            <h1 class="text-4xl sm:text-5xl font-bold text-gray-800 dark:text-white lg:text-7xl">AutoNote</h1>
+            <span class="text-lg lg:text-2xl"><img alt="GitHub release (latest by date)" src="https://img.shields.io/github/v/release/noriban/autonote?label=%20&style=flat-squar"></span>
+        </div>
+    <p class="mt-3 text-xl text-gray-500 dark:text-gray-300 lg:text-3xl">
+        Your car companion
+    </p>
+    <a href='http://play.google.com/store/?pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+        <img class="gplay items-center justify-center mx-auto h-3/4 w-3/4" alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/>
+    </a>
+    
+</div>
 </div>
 <div class="container mx-auto px-2 pt-2">
 
 
-    <div class="mt-1 grid grid-cols-1 mx-4 gap-2 md:grid-cols-2 xl:grid-cols-2 xl:gap-14 xl:items-center xl:justify-center">
+    <!-- <div class="mt-1 grid grid-cols-1 mx-4 gap-2 md:grid-cols-2 xl:grid-cols-2 xl:gap-14 xl:items-center xl:justify-center">
         <div class="info_content">
         <div class="card">
             <div class="card2">
@@ -28,6 +36,8 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
+
+
 </div>
     
