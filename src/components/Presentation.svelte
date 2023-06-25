557602560212876881098a58b87ecd97dc490419
Norbert <norbertrom01@gmail.com>
2023-06-25 20:29:22 +0000

    new section

---
+++ src/components/Presentation.svelte
@@ -0,0 +1,17 @@
+<script>
+
+
+</script>
+
+<div class="w-11/12 mx-auto bg-blue-300 rounded-3xl py-1/3 grid grid-flow-row-dense grid-cols-2 grid-rows-none">
+    <div class="text-center content-center mx-2 py-6 items-center place-self-center">
+        <p class="text-center">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus deleniti obcaecati earum exercitationem excepturi, beatae doloribus reprehenderit, architecto ipsa voluptatum dolorem, assumenda totam quisquam eum labore repellat dolorum doloremque. Voluptate?
+        </p>
+    </div>
+    <div class="text-center content-center mx-2 py-6 items-center place-self-center">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid totam, deserunt error explicabo fuga commodi quis ad vel eum nulla similique doloribus illo, delectus quo repellendus provident excepturi repudiandae ex?
+        </p>
+    </div>
+</div>
